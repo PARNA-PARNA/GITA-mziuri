71d27a127b54a769f55f96f8db20d8261703264a
Pharnavaz Khvibliani <ph.khvibliani@dressup.local>
2025-02-18 23:44:18 +0400

    ubralo faili

--- 26012025.py
+++ 26012025.py
@@ -17,5 +17,5 @@ class Human:
 human_1 = Human(firstname="nugo",last_name="svianadze",age=21,birth_place="gori")
 print(human_1.last_name)
 Human.add_address(human_1,"gori")
-human_1.add_address("tbilisi")
+human_1.add_address("tbilis")
 print(human_1.addresses)
